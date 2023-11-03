82496c66027e96887a61f15ad53775d41ac6aef3
Austin Carie <austincarie0209@gmail.com>
2023-11-03 16:05:20 -0400

    created GetById endpoint in UserController

--- ElevenNote.WebApi/Controllers/UserController.cs
+++ ElevenNote.WebApi/Controllers/UserController.cs
@@ -3,34 +3,46 @@ using ElevenNote.Models.User;
 using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ElevenNote.WebApi.Controllers
+namespace ElevenNote.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    private readonly IUserService _userService;
+    public UserController(IUserService userService)
     {
-        private readonly IUserService _userService;
-        public UserController(IUserService userService)
-        {
-            _userService = userService;
-        }
+        _userService = userService;
+    }
 
-        [HttpPost("Register")]
-        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+    [HttpPost("Register")]
+    public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+    {
+        if (!ModelState.IsValid)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            return BadRequest(ModelState);
+        }
 
             var registerResult = await _userService.RegisterUserAsync(model);
-            if (registerResult)
-            {
-                TextResponse response = new("User was registered.");
-                return Ok(response);
-            }
-
-            return BadRequest(new TextResponse("User could not be registered."));
+        if (registerResult)
+        {
+            TextResponse response = new("User was registered.");
+            return Ok(response);
         }
+
+        return BadRequest(new TextResponse("User could not be registered."));
+    }
+
+    [HttpGet("{userId:int}")]
+    public async Task<IActionResult> GetById([FromRoute] int userId)
+    {
+        UserDetail? detail = await _userService.GetUserByIdAsync(userId);
+
+        if (detail is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(detail);
     }
 }
+
