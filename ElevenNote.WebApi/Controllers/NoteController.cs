243af061b1eba21a14e1c38f3a8ae918dc42a2d9
Austin Carie <austincarie0209@gmail.com>
2023-11-11 15:58:24 -0500

    GetNoteById controller method

--- ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -1,3 +1,5 @@
+using ElevenNote.Models.Note;
+using ElevenNote.Models.Responses;
 using ElevenNote.Services.Note;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,11 +17,39 @@ public class NoteController : ControllerBase
         _noteService = noteService;
     }
 
+    //* POST api/Note
+    [HttpPost]
+    public async Task<IActionResult> CreateNote([FromBody] NoteCreate request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var response = await _noteService.CreateNoteAsync(request);
+        if (response is not null)
+            return Ok(response);
+
+        return BadRequest(new TextResponse("Could not create note."));
+    }
+    
     //* GET api/Note
     [HttpGet]
     public async Task<IActionResult> GetAllNotes()
     {
         var notes = await _noteService.GetAllNotesAsync();
         return Ok(notes);
+    }
+
+    //* GET api/Note/5
+    [HttpGet("{noteId:int}")]
+    public async Task<IActionResult> GetNoteById([FromRoute] int noteId)
+    {
+        NoteDetail? detail = await _noteService.GetNoteByIdAsync(noteId);
+
+        //* Similar to our service method, we're using a ternary to determine our return type
+        //* If the returned value (detail) is not null, we'll return it inside a 200 OK
+        //* Otherwise we'll return a NotFound() 404 reponse
+        return detail is not null
+            ? Ok(detail)
+            : NotFound();
     }
 }
