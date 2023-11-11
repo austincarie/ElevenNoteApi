9cec88a38c5cb5e672bd26d831adf380ee3a40a7
Austin Carie <austincarie0209@gmail.com>
2023-11-11 15:32:09 -0500

    CreateNote controller method

--- ElevenNote.Services/Note/INoteService.cs
+++ ElevenNote.Services/Note/INoteService.cs
@@ -3,5 +3,6 @@ using ElevenNote.Models.Note;
 namespace ElevenNote.Services.Note;
 public interface INoteService
 {
+        Task<NoteListItem?> CreateNoteAsync(NoteCreate request);
         Task<IEnumerable<NoteListItem>> GetAllNotesAsync();
 }
